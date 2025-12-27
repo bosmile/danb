d0def9d493329bbb52567753f24ed4c6b4fa644d
Tran Hai Ha <hadptw2@gmail.com>
2025-12-27 13:26:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6183366381-f15fd",
+  "appId": "1:866741763698:web:f8edddf0aa62bde6e2cceb",
+  "apiKey": "AIzaSyBtR2SscBC1Gl6_4WRoD7TDZp3eInwcqEY",
+  "authDomain": "studio-6183366381-f15fd.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "866741763698"
+};
