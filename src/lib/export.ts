7451a07929b4deb42443fcb4f60c219a0f067548
Tran Hai Ha <hadptw2@gmail.com>
2025-12-28 03:40:52 +0000

    font chữ lỗi và chưa tự xuống dòng

--- src/lib/export.ts
+++ src/lib/export.ts
@@ -1,6 +1,5 @@
 'use client';
 
-import * as XLSX from 'xlsx';
 import jsPDF from 'jspdf';
 import 'jspdf-autotable';
 import { format } from 'date-fns';
@@ -9,14 +8,6 @@ import { format } from 'date-fns';
 interface jsPDFWithAutoTable extends jsPDF {
   autoTable: (options: any) => jsPDF;
 }
-
-export const exportToExcel = (data: any[], fileName: string, sheetName: string): void => {
-  const ws = XLSX.utils.json_to_sheet(data);
-  const wb = XLSX.utils.book_new();
-  XLSX.utils.book_append_sheet(wb, ws, sheetName);
-  const excelFileName = `${fileName}_${format(new Date(), 'yyyyMMdd')}.xlsx`;
-  XLSX.writeFile(wb, excelFileName);
-};
 
 const formatCurrency = (value: number) => {
     return new Intl.NumberFormat('vi-VN', { minimumFractionDigits: 0, maximumFractionDigits: 0 }).format(value);
